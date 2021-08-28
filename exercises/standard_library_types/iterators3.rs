fb374a9c13d80d9359957564e8018dc25dbcb386
AlbertoCaballero <albertocaballero80@outlook.com>
2021-08-28 02:53:18 -0500

    last iterators

--- exercises/standard_library_types/iterators3.rs
+++ exercises/standard_library_types/iterators3.rs
@@ -7,7 +7,6 @@
 // Execute `rustlings hint iterators3` to get some hints!
 // Have fun :-)
 
-// I AM NOT DONE
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum DivisionError {
@@ -24,7 +23,15 @@ pub struct NotDivisibleError {
 // This function should calculate `a` divided by `b` if `a` is
 // evenly divisible by b.
 // Otherwise, it should return a suitable error.
-pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {}
+pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
+    if b == 0 {
+        Err(DivisionError::DivideByZero)
+    } else if a % b == 0 {
+        Ok(a / b)
+    } else {
+        Err(DivisionError::NotDivisible(NotDivisibleError { dividend: a, divisor: b}))
+    }
+}
 
 #[cfg(test)]
 mod tests {
